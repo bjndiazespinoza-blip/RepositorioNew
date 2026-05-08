f102de929efff439b4eae35b1b4597148f786140
Benjamin Diaz <bjndiazespinoza@gmail.com>
2026-05-08 11:07:45 -0400

    Primer Ejercicio

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # RepositorioNew
+
+# Ejercicios JavaScript Básico
+
+Este repositorio contiene ejercicios de:
+
+- Variables
+- Operadores
+- Comparaciones
+- Operadores lógicos
+
+## Autor
+Tu Nombre
---
+++ ejercicio.js
@@ -0,0 +1,91 @@
+// 1
+let nombre = "Benjamin";
+let apellido = "Diaz";
+
+console.log(nombre + " " + apellido);
+
+
+// 2
+let numero = 10;
+
+console.log((numero + 5) * 3);
+
+
+// 3
+let comidaFavorita = "Pizza";
+
+console.log(comidaFavorita);
+
+
+// 4
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a % b);
+
+// Strings
+a = "10";
+b = "3";
+
+console.log(a + b);
+console.log(a - b);
+
+// String y number
+a = "10";
+b = 3;
+
+console.log(a + b);
+console.log(a - b);
+
+
+// 5
+let edad = 15;
+
+console.log(edad > 10);
+console.log(edad < 18);
+
+
+// 6
+numero = 8;
+
+console.log(numero > 5 && numero < 10);
+
+
+// 7
+let nota = 6;
+
+console.log(nota === 7);
+
+
+// 8
+let temperatura = 25;
+
+console.log(temperatura >= 30);
+
+
+// 9
+let numero1 = 10;
+let numero2 = 7;
+
+console.log(numero1 === numero2);
+console.log(numero1 > numero2);
+
+
+// 10
+edad = 17;
+let tienePermiso = true;
+
+console.log(edad >= 18 || tienePermiso);
+
+
+// 11
+let usuario = "admin";
+let password = "1234";
+
+console.log("usuario es 'admin' Y password es '1234'");
+
+
+
